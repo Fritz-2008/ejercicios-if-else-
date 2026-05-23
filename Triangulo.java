68eae1edec443c86ec7ecac768c0ca5c8ebb87d7
Fritz-2008 <fritzanimaciones@gmail.com>
2026-05-23 10:20:37 -0500

    Create Triangulo.java

---
+++ Triangulo.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a) {
+            if (a == b && b == c) {
+                System.out.println("Equilátero");
+            } else if (a == b || a == c || b == c) {
+                System.out.println("Isósceles");
+            } else {
+                System.out.println("Escaleno");
+            }
+        } else {
+            System.out.println("Inválido");
+        }
+    }
+}
